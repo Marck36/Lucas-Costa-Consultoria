0dff1aa7bb8f695271d203667b6029d8546334ef
Marckms26 <74006142+Marckms26@users.noreply.github.com>
2024-10-29 12:06:42 -0300

    criacao do projeto angular

---
+++ src/app/modules/consultancy.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consultancy',
+  templateUrl: './consultancy.component.html',
+  styleUrl: './consultancy.component.scss'
+})
+export class ConsultancyComponent {
+  public title: string = 'Lucas-Costa-Consultoria';
+}
---
+++ src/app/modules/consultancy.module.ts
@@ -0,0 +1,15 @@
+
+import { NgModule } from '@angular/core';
+
+import { HeaderComponent } from "./consultancy/components";
+
+@NgModule({
+    declarations: [
+        HeaderComponent,
+    ],
+    imports: [],
+    exports: [],
+    providers: [],
+
+})
+export class ConsultancyModule { }
