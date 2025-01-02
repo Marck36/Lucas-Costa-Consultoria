c93c8d1581a8c2921de1c745792ff4db966ce4be
Marck36 <marcus.mariano.silva@gmail.com>
2025-01-02 09:33:54 -0300

    ajustando router-outlet e iniciando adaptacao container

--- src/app/modules/consultancy/consultancy-routing.module.ts
+++ src/app/modules/consultancy/consultancy-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
+import { HomeComponent } from "./pages/home/home.component";
 import { ConsultancyComponent } from './consultancy.component';
 
 const routes: Routes = [
   {
     path: '', component: ConsultancyComponent,
     children: [
-      { path: '', component: ConsultancyComponent, },
+      { path: '', component: HomeComponent, },
+      { path: 'home', component: HomeComponent, },
+      { path: 'consultancy', component: ConsultancyComponent, },
     ]
   },
 ];
